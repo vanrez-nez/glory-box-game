311cac2840066b28b392507e886f11903aab1f3f
Iván Juárez <ivan@blindgazer.com>
2018-05-30 03:23:59 -0500

    refact: moving steering trails effect into a separated file

--- src/game/player-hud.js
+++ src/game/player-hud.js
@@ -1,106 +1,35 @@
 import { GAME, EVENTS } from './const';
 import { GetScreenCoords } from './utils';
+import GameSteeringTrailsSfx from './sfx/steering-trails-sfx';
 import { MaterialFactoryInstance as MaterialFactory } from './materials/material-factory';
-import SteeringParticle from './steering-particle';
-import LineTrail from './line-trail';
 
 const DEFAULT = {
   camera: null,
 };
-
-const cachedVec = new THREE.Vector3();
 
 export default class GamePlayerHud {
   constructor(opts) {
     this.opts = { ...DEFAULT, ...opts };
     this.events = new EventEmitter3();
     this.group = new THREE.Group();
-    this.trailsActive = false;
-    this.tracers = [];
+    this.trailsSfx = new GameSteeringTrailsSfx({
+      parent: this.opts.camera,
+    });
     this.loadModel();
-    this.initTrails();
     this.addFireballMesh();
+    this.attachEvents();
   }
 
-  /* 
-    Trails for collectible pickup sfx
-  */
-  initTrails() {
-    const { camera } = this.opts;
-    this.trails = [];
-    for (let i = 0; i < 5; i++) {
-      const trail = new LineTrail({
-        maxPositions: 20,
-        material: MaterialFactory.getMaterial('CollectibleTrail', {
-          name: 'ph_trail',
-          color: 0xffffff,
-          lineWidth: 0.5,
-        }, 'hud-trails'),
-      });
-      this.trails.push(trail);
-      camera.add(trail.mesh);
-    }
+  attachEvents() {
+    const { trailsSfx } = this;
+    trailsSfx.events.on(EVENTS.SteeringTrailLanded, this.onSteeringTrailLanded.bind(this));
   }
 
-  /* 
-    Start trails sfx copying the given positions and color.
-  */
-  spawnTrailsFrom(positions, color) {
-    const { camera } = this.opts;
-    const { trails } = this;
-    this.trailsActive = true;
-    this.tracers = [];
-    const rotStep = Math.PI / trails.length;
-    for (let i = 0; i < trails.length; i++) {
-      const trail = trails[i];
-      const position = positions[i];
-      trail.mesh.visible = true;
-      const p = new SteeringParticle({});
-      // Give a start acceleration to particle
-      const x = Math.sin(rotStep * i) * 2;
-      const y = Math.cos(rotStep * i) * 2;
-      const z = 1;
-      p.acceleration.set(x, y, z);
-      // Translate position to local coords inside the camera
-      camera.worldToLocal(position);
-      p.position.copy(position);
-      trail.resetPositionTo(position);
-      trail.mesh.material.uniforms.color.value.setHex(color);
-      this.tracers.push({
-        trail,
-        particle: p,
-      });
-    }
-  }
-
-  updateTrails() {
-    const { tracers, fireball } = this;
-    let idx = tracers.length;
-    while (idx--) {
-      const { trail, particle } = tracers[idx];
-      cachedVec.copy(fireball.position);
-      cachedVec.z -= 0.3;
-      particle.update();
-      particle.seek(cachedVec);
-      if (particle.position.distanceTo(cachedVec) < 0.01) {
-        trail.mesh.visible = false;
-        tracers.splice(idx, 1);
-        if (tracers.length === 0) {
-          const { color } = trail.mesh.material.uniforms;
-          this.emitCollectEvent(color.value.clone());
-        }
-      } else {
-        trail.pushPosition(particle.position);
-      }
-    }
-  }
-
-  emitCollectEvent(color) {
+  onSteeringTrailLanded(color) {
     this.events.emit(EVENTS.CollectibleCollect, color);
   }
 
-  loadModel() {
-  }
+  loadModel() {}
 
   addFireballMesh() {
     const geo = new THREE.SphereBufferGeometry(0.6, 10, 10);
@@ -131,6 +60,10 @@ export default class GamePlayerHud {
     }, 0);
   }
 
+  spawnTrailsFrom(positions, color) {
+    this.trailsSfx.spawnTrailsFrom(positions, color);
+  }
+
   resetPower(tl) {
     const { fireball } = this;
     tl.to(fireball.scale, 0.5, {
@@ -147,6 +80,6 @@ export default class GamePlayerHud {
     const [x, y] = GetScreenCoords(0.03, 0.5, camera, 20);
     fireball.position.set(x, y, -GAME.HudDistanceFromCamera);
     uniforms.time.value += delta * 0.15;
-    this.updateTrails();
+    this.trailsSfx.update(fireball.position);
   }
 }
---
+++ src/game/sfx/steering-trails-sfx.js
@@ -0,0 +1,102 @@
+import { EVENTS } from '../const';
+import { MaterialFactoryInstance as MaterialFactory } from '../materials/material-factory';
+import SteeringParticle from '../steering-particle';
+import LineTrail from '../line-trail';
+
+const cachedVec = new THREE.Vector3();
+const DEFAULT = {
+  parent: null,
+};
+
+export default class GameSteeringTrailsSfx {
+  constructor(opts) {
+    this.opts = { ...DEFAULT, ...opts };
+    this.events = new EventEmitter3();
+    this.tracers = [];
+    this.active = false;
+    this.initTrails();
+  }
+
+  emitLandedEvent(color) {
+    this.events.emit(EVENTS.SteeringTrailLanded, color);
+  }
+
+  /*
+    Trails for collectible pickup sfx
+  */
+  initTrails() {
+    const { parent } = this.opts;
+    this.trails = [];
+    for (let i = 0; i < 5; i++) {
+      const trail = new LineTrail({
+        maxPositions: 20,
+        material: MaterialFactory.getMaterial('CollectibleTrail', {
+          name: 'ph_trail',
+          color: 0xffffff,
+          lineWidth: 0.5,
+        }, 'hud-trails'),
+      });
+      this.trails.push(trail);
+      parent.add(trail.mesh);
+    }
+  }
+
+  /*
+    Start trails sfx copying the given positions and color.
+  */
+  spawnTrailsFrom(positions, color) {
+    const { parent } = this.opts;
+    const { trails } = this;
+    this.active = true;
+    this.tracers = [];
+    const rotStep = Math.PI / trails.length;
+    for (let i = 0; i < trails.length; i++) {
+      const trail = trails[i];
+      const position = positions[i];
+      trail.mesh.visible = true;
+      const p = new SteeringParticle({});
+      // Give a start acceleration to particle
+      const x = Math.sin(rotStep * i) * 2;
+      const y = Math.cos(rotStep * i) * 2;
+      const z = 1;
+      p.acceleration.set(x, y, z);
+      // Translate position to local coords inside the parent
+      parent.worldToLocal(position);
+      p.position.copy(position);
+      trail.resetPositionTo(position);
+      trail.mesh.material.uniforms.color.value.setHex(color);
+      this.tracers.push({
+        trail,
+        particle: p,
+      });
+    }
+  }
+
+  update(targetPosition) {
+    const { tracers } = this;
+    let idx = tracers.length;
+
+    if (!this.active) {
+      return;
+    }
+
+    while (idx--) {
+      const { trail, particle } = tracers[idx];
+      cachedVec.copy(targetPosition);
+      cachedVec.z -= 0.3;
+      particle.update();
+      particle.seek(cachedVec);
+      if (particle.position.distanceTo(cachedVec) < 0.01) {
+        trail.mesh.visible = false;
+        tracers.splice(idx, 1);
+        if (tracers.length === 0) {
+          const { color } = trail.mesh.material.uniforms;
+          this.emitLandedEvent(color.value.clone());
+          this.active = false;
+        }
+      } else {
+        trail.pushPosition(particle.position);
+      }
+    }
+  }
+}
