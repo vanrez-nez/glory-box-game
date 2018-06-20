5a1a646ac12ac424de8891fcb992e57396057312
Iván Juárez <ivan@blindgazer.com>
2018-06-20 01:37:03 -0500

    feat: new mood colors

--- src/game/mood-presets/default.js
+++ src/game/mood-presets/default.js
@@ -19,39 +19,39 @@ const DefaultMood = {
     Low: {
       player_main: {
         color: 16777215,
-        reflectivity: 1
+        reflectivity: 1,
       },
       player_particle: {
         color: 4544895,
-        reflectivity: 1
+        reflectivity: 1,
       },
       explode_trail: {
         color: 16777215,
-        lineWidth: 0.7076244550096792
+        lineWidth: 0.7076244550096792,
       },
       enemy_ray: {
-        rayColor: 2972098,
-        thinDebrisColor: 1996743,
-        fatDebrisColor: 38399
+        rayColor: 3484866,
+        thinDebrisColor: 1922524,
+        fatDebrisColor: 8782079,
       },
       w_floor: {
         color: 4148873,
-        reflectivity: 1
+        reflectivity: 1,
       },
       w_main_cylinder: {
         color: 2569823,
-        reflectivity: 0.5
+        reflectivity: 0.5,
       },
       w_fx_cylinder: {
-        opacity: 1
+        opacity: 1,
       },
       w_base_cylinder: {
         color: 2569823,
-        reflectivity: 0.5
+        reflectivity: 0.5,
       },
       ph_trail: {
         color: 16731983,
-        lineWidth: 0.3
+        lineWidth: 0.25,
       },
       ph_fireball: {
         fissuresColor : 16730880,
@@ -66,143 +66,143 @@ const DefaultMood = {
       },
       enemy_head: {
         color: 1397676,
-        reflectivity: 1
+        reflectivity: 1,
       },
       enemy_eyes: {
         color: 16056363,
-        reflectivity: 1
+        reflectivity: 1,
       },
       enemy_armor: {
         color: 1658505,
-        reflectivity: 1
+        reflectivity: 1,
       },
       enemy_col_trail: {
         color: 1912681,
-        lineWidth: 2
+        lineWidth: 2,
       },
       plt_steps: {
         color: 724244,
-        reflectivity: 1
+        reflectivity: 1,
       },
       plt_static: {
-        color: 5039856,
-        reflectivity: 1
+        color: 38655,
+        reflectivity: 1,
       },
       collect_glyph_4: {
         color: 592913,
         emissive: 16645935,
         emissiveIntensity: 6,
         reflectivity: 0.5,
-        refractionRatio: 0.98
+        refractionRatio: 0.98,
       },
       collect_item_4: {
         color: 16645935,
-        reflectivity: 1
+        reflectivity: 1,
       },
       collect_trail_4: {
         color: 16645935,
-        lineWidth: 0.3
+        lineWidth: 0.3,
       },
       collect_glyph_2: {
         color: 592913,
         emissive: 3140095,
         emissiveIntensity: 6,
         reflectivity: 0.5,
-        refractionRatio: 0.98
+        refractionRatio: 0.98,
       },
       collect_item_2: {
         color: 3140095,
-        reflectivity: 1
+        reflectivity: 1,
       },
       collect_trail_2: {
         color: 3140095,
-        lineWidth: 0.3
+        lineWidth: 0.3,
       },
       plt_dynamic: {
-        color: 0xffb400,
-        reflectivity: 1
+        color: 16758608,
+        reflectivity: 1,
       },
       collect_glyph_5: {
         color: 592913,
         emissive: 16731983,
         emissiveIntensity: 6,
         reflectivity: 0.5,
-        refractionRatio: 0.98
+        refractionRatio: 0.98,
       },
       collect_item_5: {
         color: 16731983,
-        reflectivity: 1
+        reflectivity: 1,
       },
       collect_trail_5: {
         color: 16731983,
-        lineWidth: 0.3
+        lineWidth: 0.3,
       },
       collect_glyph_6: {
         color: 592913,
         emissive: 16731983,
         emissiveIntensity: 6,
         reflectivity: 0.5,
-        refractionRatio: 0.98
+        refractionRatio: 0.98,
       },
       plt_socket: {
         color: 395793,
-        reflectivity: 1
+        reflectivity: 1,
       },
       cl_socket: {
         color: 461073,
-        reflectivity: 1
+        reflectivity: 1,
       },
       collect_glyph_1: {
         color: 592913,
         emissive: 15937535,
         emissiveIntensity: 6,
         reflectivity: 0.5,
-        refractionRatio: 0.98
+        refractionRatio: 0.98,
       },
       collect_item_1: {
         color: 15937535,
-        reflectivity: 1
+        reflectivity: 1,
       },
       collect_trail_1: {
         color: 15937535,
-        lineWidth: 0.3
+        lineWidth: 0.3,
       },
       collect_glyph_8: {
         color: 592913,
         emissive: 16731983,
         emissiveIntensity: 6,
         reflectivity: 0.5,
-        refractionRatio: 0.98
+        refractionRatio: 0.98,
       },
       collect_glyph_0: {
         color: 592913,
         emissive: 16731983,
         emissiveIntensity: 6,
         reflectivity: 0.5,
-        refractionRatio: 0.98
+        refractionRatio: 0.98,
       },
       collect_glyph_7: {
         color: 592913,
         emissive: 16731983,
         emissiveIntensity: 6,
         reflectivity: 0.5,
-        refractionRatio: 0.98
+        refractionRatio: 0.98,
       },
       collect_glyph_3: {
         color: 592913,
         emissive: 16747567,
         emissiveIntensity: 6,
         reflectivity: 0.5,
-        refractionRatio: 0.98
+        refractionRatio: 0.98,
       },
       collect_item_3: {
         color: 16747567,
-        reflectivity: 1
+        reflectivity: 1,
       },
       collect_trail_3: {
         color: 16747567,
-        lineWidth: 0.3
-      }
+        lineWidth: 0.3,
+      },
     }
   },
 };
