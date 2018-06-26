457dcab3375b386909c68c65a45033feae5c0857
Iván Juárez <ivan@blindgazer.com>
2018-06-26 12:15:47 -0500

    fix: initalizing enemy vortex

--- src/game/enemy.js
+++ src/game/enemy.js
@@ -1,5 +1,6 @@
 import GameEnemyDragon from './enemy-dragon';
 import GameEnemyRays from './enemy-rays';
+import GameEnemyVortex from './enemy-vortex';
 
 const DEFAULT = {};
 
@@ -10,12 +11,14 @@ export default class GameEnemy {
     this.group.name = 'GameEnemy';
     this.dragon = new GameEnemyDragon({ parent: this.group });
     this.rays = new GameEnemyRays({ parent: this.group });
+    this.vortex = new GameEnemyVortex({ parent: this.group });
     this.bodies = this.rays.bodies.concat(this.dragon.body);
   }
 
   update(delta, camera, playerPosition) {
     this.dragon.update(delta, playerPosition);
     this.rays.update(delta, camera, playerPosition);
+    this.vortex.update(delta);
   }
 
   restart() {
