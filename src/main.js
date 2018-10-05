1f8902fcddaf8aaea1bca058295af1ad1f9b6aa6
Iván Juárez <ivan@blindgazer.com>
2018-10-05 14:53:59 -0500

    chore: simplifying three shim imports

--- src/main.js
+++ src/main.js
@@ -1,8 +1,19 @@
-/*
-  Load legacy code via script-loader
-*/
+/* eslint-disable */
+
+// Load legacy code via script-loader
 import '../static/js/vendor/ThreeCSG.min';
 import '../static/js/vendor/Simple1DNoise';
+
+// Load Three as global with dependencies
 import './three-global';
-import './three-shim';
+import 'three/examples/js/postprocessing/EffectComposer';
+import 'three/examples/js/shaders/CopyShader';
+import 'three/examples/js/shaders/FXAAShader';
+import 'three/examples/js/shaders/LuminosityHighPassShader';
+import 'three/examples/js/postprocessing/RenderPass';
+import 'three/examples/js/postprocessing/ShaderPass';
+import 'three/examples/js/postprocessing/UnrealBloomPass';
+import 'three/examples/js/loaders/GLTFLoader';
+
+// Init Game
 import './game';
--- src/three-shim.js
+++
@@ -1,8 +0,0 @@
-import 'three/examples/js/postprocessing/EffectComposer';
-import 'three/examples/js/shaders/CopyShader';
-import 'three/examples/js/shaders/FXAAShader';
-import 'three/examples/js/shaders/LuminosityHighPassShader';
-import 'three/examples/js/postprocessing/RenderPass';
-import 'three/examples/js/postprocessing/ShaderPass';
-import 'three/examples/js/postprocessing/UnrealBloomPass';
-import 'three/examples/js/loaders/GLTFLoader';
