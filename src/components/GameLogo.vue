d98683d10e78c6803600c57357065c99f5d741a9
Iván Juárez <ivan@blindgazer.com>
2018-10-17 16:52:21 -0500

    feat: game logo component

---
+++ src/components/GameLogo.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="GameLogo">
+    <svg>
+      <use xlink:href="#icon-logo"></use>
+    </svg>
+    <canvas ref="canvas"></canvas>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'GameLogo',
+  data() {
+    return {};
+  },
+  methods: {},
+};
+</script>
+
+<style lang="stylus" src='@styles/components/GameLogo.styl'></style>
