c32783a284978eefc291e9416756b20342c8187e
Iván Juárez <ivan@blindgazer.com>
2018-10-17 16:54:48 -0500

    feat: basic struct for about view

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,23 @@
 <template>
   <div class="about">
+    <game-input></game-input>
     <h1>This is an about page</h1>
   </div>
 </template>
+
+<script>
+import GameInput from '@/components/GameInput';
+
+export default {
+  name: 'About',
+  components: {
+    GameInput,
+  },
+  computed: {
+    keymap() {
+      return {
+      };
+    },
+  },
+};
+</script>
