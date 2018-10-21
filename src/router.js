3b0534e03c4044c4045312e0a673934b8665e66d
Iván Juárez <ivan@blindgazer.com>
2018-10-20 23:11:10 -0500

    feat: about view without lazy loading

--- src/router.js
+++ src/router.js
@@ -1,7 +1,7 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import Home from './views/Home';
-// import Game from './views/Game';
+import Home from './views/home';
+import About from './views/about';
 
 Vue.use(Router);
 
@@ -13,17 +13,17 @@ export default new Router({
       component: Home,
     },
     {
-      path: '/game',
-      name: 'game',
-      component: () => import(/* webpackChunkName: "game" */ './views/Game'),
-    },
-    {
       path: '/about',
       name: 'about',
+      component: About,
+    },
+    {
+      path: '/game',
+      name: 'game',
       // route level code-splitting
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About'),
+      component: () => import(/* webpackChunkName: "game" */ './views/game'),
     },
   ],
 });
