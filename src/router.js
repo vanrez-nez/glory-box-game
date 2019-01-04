eed8e17e11daa08f8424e04a4181f3e7b679c131
Iván Juárez <ivan@blindgazer.com>
2019-01-03 19:18:32 -0600

    feat: validating game qualities from url

--- src/router.js
+++ src/router.js
@@ -6,6 +6,7 @@ import About from './views/about';
 Vue.use(Router);
 
 export default new Router({
+  mode: 'history',
   routes: [
     {
       path: '/',
@@ -18,12 +19,21 @@ export default new Router({
       component: About,
     },
     {
-      path: '/game',
+      path: '/game/:quality',
       name: 'game',
       // route level code-splitting
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import(/* webpackChunkName: "game" */ './views/game'),
+      beforeEnter: (to, from, next) => {
+        const { quality } = to.params;
+        // validate only valid qualities
+        if (['low', 'medium', 'high'].indexOf(quality) > -1) {
+          next();
+        } else {
+          next('/');
+        }
+      },
     },
   ],
 });
