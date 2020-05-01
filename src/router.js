876498606034b2941df7744fa355d6acaeb67298
Iván Juárez Núñez <ivan@blindgazer.com>
2020-05-01 02:58:14 -0500

    fix: view name case

--- src/router.js
+++ src/router.js
@@ -1,7 +1,7 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import Home from './views/home';
-import About from './views/about';
+import Home from './views/Home';
+import About from './views/About';
 
 Vue.use(Router);
 
